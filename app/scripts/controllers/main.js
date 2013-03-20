cad3e84ba82b46ba530a484fb8ddd66e19048013
Josh Adams <josh@isotope11.com>
2013-03-20 12:54:47 -0500

    Make clicking 'foo' add markers based on server response.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,4 +15,32 @@ angular.module('angularMapDemoApp')
         position: new google.maps.LatLng(35, 35)
       }));
     };
+    $scope.updateMarkers = function(){
+      var returned = {
+        people: [
+          {
+            name: null,
+            imei: "351746052214411",
+            lat: "36.1612292099744",
+            long: "-86.7808193620294"
+          },
+          {
+            name: null,
+            imei: "353918051258594",
+            lat: "33.47503485",
+            long: "-86.82233522"
+          }
+        ]
+      }
+      $scope.markers = extractMarkersFromPeople(returned.people);
+    };
+
+    var extractMarkersFromPeople = function(people){
+      return people.map(function(p){
+        return new google.maps.Marker({
+          map: $scope.map,
+          position: new google.maps.LatLng(p.lat, p.long)
+        });
+      });
+    };
   });
